468e6371cf57b83dd7f4b7891abdb7e9c2101ede
Youness Sahraoui <ysahraou@c4r4p8.1337.ma>
2024-03-09 17:49:20 +0100

    Set the header in pipx.h and the includegardes

--- pipex.h
+++ pipex.h
@@ -1,5 +1,21 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include "libft/libft.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ysahraou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/09 17:47:18 by ysahraou          #+#    #+#             */
+/*   Updated: 2024/03/09 17:48:41 by ysahraou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include "libft/libft.h"
+
+#endif
